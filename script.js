915fca4f61dbac8fdeb6f7d098dd78d8ceffbf7e
NGShpagin <shpagin9393@mail.ru>
2023-06-25 02:31:01 +0300

    add HW5

--- script.js
+++ script.js
@@ -71,9 +71,125 @@ console.log(`На еду желательно потратить не больш
 //     if (count === 2) alert("Пароль не верный");
 // }
 
-const word = "example";
-arrWord = word.split("");
-console.log(arrWord);
-console.log(arrWord.pop());
+// const word = "example";
+// arrWord = word.split("");
+// console.log(arrWord);
+// console.log(arrWord.pop());
+//
+// console.log(arrWord.join("").toUpperCase());
 
-console.log(arrWord.join("").toUpperCase());
+const car = {
+    make: "Audi",
+    model: "A5",
+    year: 2023,
+    color: "red",
+    passengers: 2,
+    power: 249,
+    odometer: 0,
+    engineStarted: false,
+    startEngine: function () {
+        this.engineStarted = true;
+    },
+    stopEngine: function () {
+        this.engineStarted = false;
+    },
+    haveRoadTrip: function (distance) {
+        if (this.engineStarted) this.odometer += distance;
+        else alert("Engine is off");
+    }
+}
+
+const product = {
+    name: "Table",
+    price: 2000,
+    count: 2,
+    userRegister: true,
+    register: function () {
+        this.userRegister = true;
+    },
+    countProduct: function () {
+        if (product.count > 0) console.log("Вы купили данный товар");
+        else console.log("Товара нет в наличии")
+    },
+    buyProduct: function () {
+        if (this.userRegister) console.log("ТОвар добавлен");
+        else console.log("Пользователь не зарегистрирован");
+    }
+}
+
+console.log(product);
+
+product.description = "Product description";
+
+console.log(product);
+
+delete product.description;
+
+console.log(product);
+
+let productSale = product;
+
+productSale.price = 1500;
+
+console.log("Sale: " + productSale);
+console.log("Product: " + product)
+
+const object = {
+    1: "Ivanov",
+    2: "Petrov",
+}
+
+// const students = object.map((student) => `student: ${student}`);
+console.log(Object.values(car));
+
+
+const studentPracticeTime = [
+    {
+        firstName: "Ivanov",
+        practiceTime: 56
+    },
+    {
+        firstName: "Sidorov",
+        practiceTime: 149
+    },
+    {
+        firstName: "Belkin",
+        practiceTime: 28
+    },
+    {
+        firstName: "Avdeev",
+        practiceTime: 168
+    }
+]
+
+
+// Без reduce
+let totalTime = 0;
+for (let index = 0; index < studentPracticeTime.length; index++) {
+    totalTime += studentPracticeTime[index].practiceTime;
+}
+console.log(totalTime);
+
+// some
+const studentBelkin = studentPracticeTime.find((student) => {
+    return student.firstName === "Belkin";
+})
+
+
+const array = [1, 2, 3, 10, 15]
+
+// map
+const arrayMap = array.map(item => item * 2);
+console.log(arrayMap);
+
+// filter
+const arrayFilter = array.filter(item => item >= 10);
+console.log(arrayFilter);
+
+// some
+const arraySome = array.some(item => item >= 10);
+console.log(arraySome);
+
+// reduce
+const arrayReduce = array.reduce((a, b) => a + b);
+console.log(arrayReduce);
